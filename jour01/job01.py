f031f0c4267ae09390c3502473bf1039b5525d01
adam-messaoudi <adam.messaoudi@laplateforme.io>
2024-01-04 10:15:20 +0100

    ajout dossier  et fichier

--- jour01/job01.py
+++
@@ -1,9 +0,0 @@
-# Définition de la classe Operation
-class Operation:
-    def __init__(self, nombre1=0, nombre2=0):
-        self.nombre1 = nombre1  # Initialisation de l'attribut nombre1
-        self.nombre2 = nombre2  # Initialisation de l'attribut nombre2
-
-# Instanciation de la classe et impression de l'objet
-operation_instance = Operation()
-print(operation_instance)
