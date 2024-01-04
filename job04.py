9772281180b7a123609785d2f769643259177304
adam-messaoudi <adam.messaoudi@laplateforme.io>
2024-01-04 09:55:07 +0100

    ajout dossier  et fichier

--- job04.py
+++ job04.py
@@ -3,25 +3,43 @@ class Student:
         self.__nom = nom
         self.__prenom = prenom
         self.__numero_etudiant = numero_etudiant
-        self.__credits = 0  # Attribut privé credits par défaut à zéro
+        self.__credits = 0
+        self.__level = self.__studentEval()
     
-    # Mutateur pour ajouter des crédits avec validation
     def add_credits(self, amount):
         if amount > 0:
             self.__credits += amount
+            self.__level = self.__studentEval()
         else:
             print("La valeur des crédits ajoutés doit être supérieure à zéro.")
     
-    # Méthode pour obtenir le total de crédits de l'étudiant
+    def __studentEval(self):
+        if self.__credits >= 90:
+            return "Excellent"
+        elif self.__credits >= 80:
+            return "Très bien"
+        elif self.__credits >= 70:
+            return "Bien"
+        elif self.__credits >= 60:
+            return "Moyen"
+        else:
+            return "Insuffisant"
+    
+    def studentInfo(self):
+        print(f"Nom de l'étudiant : {self.__nom}")
+        print(f"Prénom de l'étudiant : {self.__prenom}")
+        print(f"id de l'étudiant : {self.__numero_etudiant}")
+        print(f"Niveau de l'étudiant : {self.__level}")
+    
+    # Getter pour récupérer le nombre de crédits
     def get_credits(self):
         return self.__credits
 
-# Instanciation de l'étudiant John Doe avec le numéro d'étudiant 145
 john_doe = Student("John", "Doe", 145)
-
-# Ajout de crédits à l'étudiant John Doe et impression du total de crédits
-john_doe.add_credits(20)
+john_doe.add_credits(10)
 john_doe.add_credits(30)
 john_doe.add_credits(40)
 
-print("Total de crédits de l'étudiant John Doe est de :", john_doe.get_credits(),"points")
+# Affichage du total de crédits et des informations de l'étudiant
+print("Total de crédits de l'étudiant John Doe :", john_doe.get_credits())
+john_doe.studentInfo()
