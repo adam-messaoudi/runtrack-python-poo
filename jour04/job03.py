3c72ddf5a42887e9b5842c926818b1e564a52d93
adam-messaoudi <adam.messaoudi@laplateforme.io>
2024-01-07 19:12:06 +0100

    ajout job03

--- jour04/job03.py
+++ jour04/job03.py
@@ -1,57 +1,59 @@
-class Rectangle:
+class Forme:
+    def aire(self):
+        # Méthode par défaut renvoyant une aire nulle
+        return 0
+
+class Rectangle(Forme):
     def __init__(self, longueur, largeur):
-        # Initialisation des attributs privés longueur et largeur
+        # Initialisation des attributs longueur et largeur
         self.__longueur = longueur
         self.__largeur = largeur
 
-    # Assesseurs pour la longueur et la largeur
+    # Getters et setters pour longueur et largeur
     def get_longueur(self):
         return self.__longueur
+
+    def set_longueur(self, longueur):
+        self.__longueur = longueur
 
     def get_largeur(self):
         return self.__largeur
 
-    # Mutateurs pour la longueur et la largeur
-    def set_longueur(self, nouvelle_longueur):
-        self.__longueur = nouvelle_longueur
+    def set_largeur(self, largeur):
+        self.__largeur = largeur
 
-    def set_largeur(self, nouvelle_largeur):
-        self.__largeur = nouvelle_largeur
-
-    # Méthode pour calculer le périmètre du rectangle
+    # Méthodes pour calculer le périmètre et la surface du rectangle
     def perimetre(self):
         return 2 * (self.__longueur + self.__largeur)
 
-    # Méthode pour calculer la surface du rectangle
     def surface(self):
         return self.__longueur * self.__largeur
 
-
 class Parallelepipede(Rectangle):
     def __init__(self, longueur, largeur, hauteur):
-        # Appel du constructeur de la classe parent Rectangle
         super().__init__(longueur, largeur)
-        # Initialisation de l'attribut privé hauteur
         self.__hauteur = hauteur
 
-    # Assesseur pour la hauteur
+    # Getter et setter pour la hauteur
     def get_hauteur(self):
         return self.__hauteur
 
-    # Mutateur pour la hauteur
-    def set_hauteur(self, nouvelle_hauteur):
-        self.__hauteur = nouvelle_hauteur
+    def set_hauteur(self, hauteur):
+        self.__hauteur = hauteur
 
     # Méthode pour calculer le volume du parallélépipède
     def volume(self):
-        return self.__hauteur * self.surface()
+        return self.surface() * self.__hauteur
 
+# Instanciation d'un rectangle et d'un parallélépipède pour les tests
+rectangle1 = Rectangle(2, 44)
+print("Perimètre du rectangle :", rectangle1.perimetre())
+print("Surface du rectangle :", rectangle1.surface())
 
-# Instanciation de la classe Rectangle
-rectangle = Rectangle(5, 10)
-print("Périmètre du rectangle:", rectangle.perimetre())
-print("Surface du rectangle:", rectangle.surface())
+rectangle1.set_longueur(5)
+rectangle1.set_largeur(68)
+print("Perimètre du rectangle après la modif :", rectangle1.perimetre())
+print("Surface du rectangle après la modif :", rectangle1.surface())
 
-# Instanciation de la classe Parallelepipede
-parallelepipede = Parallelepipede(3, 4, 6)
-print("Volume du parallélépipède:", parallelepipede.volume())
+parallelepipede1 = Parallelepipede(1, 2, 3)
+print("Volume du parallelepipede :", parallelepipede1.volume())
